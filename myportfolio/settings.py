f9de4bd6e504df786819bbd74ce29a029bfac35c
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2021-09-10 22:19:41 +0100

    updated settings

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -129,7 +129,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = 587
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 SERVER_EMAIL = EMAIL_HOST_USER
