b107c5303ddabf8ae4a7d25b368075059aa907f1
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2021-09-11 13:53:20 +0100

    change venv

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
